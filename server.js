d2fd51a5e482ba5281ca22e4960caa4a301c022c
verixx <abdurraqeeb53@gmail.com>
2018-03-20 15:13:03 +1100

    config

--- server.js
+++ server.js
@@ -2,8 +2,6 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
-
-// respond with "hello world" when a GET request is made to the homepage
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + '/index.html'))
 })
