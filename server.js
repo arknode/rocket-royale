f18673f673497bf0dd67a59f76cf20e12d8644e1
Jason Yu <jasonyu0100@gmail.com>
2018-03-20 21:53:15 +1100

    changed stuff

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require('express')
 const path = require('path')
 const app = express()
+const server = require('http').Server(app)
+const io = require('socket.io')(server);
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + '/index.html'))
 })
+
+io.on('connection', function(socket) {
+	console.log('a user connected');
+});
 
 app.listen(8000, () => {
     console.log('Online at http://localhost:8000')
